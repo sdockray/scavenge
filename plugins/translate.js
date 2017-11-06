a3c5dae02698fac436bfda00ce49176a2da6dca4
e-e-e <benjamin.mforster@gmail.com>
2017-11-06 23:25:11 +1100

    make translate work with arrays too.

--- plugins/translate.js
+++ plugins/translate.js
@@ -7,8 +7,12 @@ function convertMatchToString (match, template) {
 }
 
 function translate (data, config) {
+  // for each variable in config
   _.each(config, (optionList, variable) => {
+    // get value to translate
     const toTranslate = data[variable]
+    // if it is a string, iterate over each and make new
+    // DO THIS BETTER SMARTER
     if (toTranslate && typeof toTranslate === 'string') {
       optionList.forEach((options) => {
         var re = new RegExp(options.match)
@@ -20,6 +24,15 @@ function translate (data, config) {
         } else {
           console.log('no match for', options.match, 'in', toTranslate)
         }
+      })
+    } else if (toTranslate && Array.isArray(toTranslate)) {
+      optionList.forEach((options) => {
+        var re = new RegExp(options.match)
+        var matches = toTranslate.map(v => v.match(re))
+        console.log(variable, matches)
+        _.each(options.to, (translator, newVariable) => {
+          data[newVariable] = matches.map(match => match ? convertMatchToString(match, translator) : undefined)
+        })
       })
     } else {
       console.log('warning', variable, 'is was not found and cannot be translated')

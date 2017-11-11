c1e3fc85b6da534fe512e8509526da3fb25f3a2c
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2017-11-11 12:35:01 +1100

    refactor goAction and goScrape into single go action.

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 var scavenge = require('./lib/scavenge')
 
-// Instructions are a JSON object describing the scavenge task
-function go (instructions, data) {
-  // require action plugins
-  if (data) {
-    scavenge.goActions(instructions, data)
-  } else {
-    scavenge.goScrape(instructions)
-  }
-}
-
-module.exports = go
+module.exports = scavenge.go

7ed40db75ecefd8a6e36da375303ee80b9e43a43
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2017-11-11 23:26:17 +1100

    fix tests - 96% coverage of translations

--- plugins/translate.js
+++ plugins/translate.js
@@ -10,14 +10,18 @@ function translateVariable (data, input, options) {
   let output = input
   if (output === undefined) return options.default
   const re = options.match && new RegExp(options.match)
+
   _.each(options.to, (translator, newVariable) => {
     if (options.match) {
-      const matched = input.match(re)
-      if (matched) {
-        data[newVariable] = convertMatchToString(matched, translator)
+      if (Array.isArray(input)) {
+        // iterate over
+        data[newVariable] = input.map((value) => {
+          const matched = value.match(re)
+          return matched ? convertMatchToString(matched, translator) : options.default;
+        })
       } else {
-        console.log('no match for', options.match, 'in', input)
-        data[newVariable] = options.default
+        const matched = input.match(re)
+        data[newVariable] = (matched) ? convertMatchToString(matched, translator) : options.default
       }
     }
   })
@@ -31,25 +35,25 @@ function translate (data, config) {
     // get value/s to translate
     const toTranslate = data[variable]
     // if it is a string, iterate over each and make new
-    if (toTranslate === undefined || typeof toTranslate === 'string') {
-      if (Array.isArray(optionList)) {
-        optionList.forEach((options) => {
-          data[variable] = translateVariable(data, toTranslate, options)
-        })
-      } else {
-        data[variable] = translateVariable(data, toTranslate, optionList)
-      }
-    } else if (toTranslate && Array.isArray(toTranslate)) {
-      if (Array.isArray(optionList)) {
-        optionList.forEach((options) => {
-          data[variable] = toTranslate.map(value => translateVariable(data, value, options))
-        })
-      } else {
-        data[variable] = toTranslate.map(value => translateVariable(data, value, optionList))
-      }
+    // if (toTranslate === undefined || typeof toTranslate === 'string') {
+    if (Array.isArray(optionList)) {
+      optionList.forEach((options) => {
+        data[variable] = translateVariable(data, toTranslate, options)
+      })
     } else {
-      console.log('warning', variable, 'is was not found and cannot be translated')
+      data[variable] = translateVariable(data, toTranslate, optionList)
     }
+    // } else if (toTranslate && Array.isArray(toTranslate)) {
+    //   if (Array.isArray(optionList)) {
+    //     optionList.forEach((options) => {
+    //       data[variable] = toTranslate.map(value => translateVariable(data, value, options))
+    //     })
+    //   } else {
+    //     data[variable] = toTranslate.map(value => translateVariable(data, value, optionList))
+    //   }
+    // } else {
+    //   console.log('warning', variable, 'is was not found and cannot be translated')
+    // }
   })
   return data
 }

0fa9a6d1eafb24082962b30f16d73eefb95148fa
e-e-e <benjamin.mforster@gmail.com>
2017-11-26 18:27:30 +1100

    update opf plugin

--- plugins/opf.js
+++ plugins/opf.js
@@ -5,14 +5,48 @@ const OPF = require('open-packaging-format')
 var utils = require('../lib/utils')
 var tpl = utils.tpl
 
+// simple assignment options
+const properties = [
+  'cover',
+  'description',
+  'type',
+  'format',
+  'coverage',
+  'rights',
+  'source',
+  'title',
+  'authors',
+  'contributors',
+  'subjects',
+  'publishers',
+  'languages'
+]
+
 function makeOpf (data, config) {
   return new Promise((resolve, reject) => {
     const dir = tpl(config.directory, data)
     const file = config.filename ? tpl(config.filename, data) : 'metadata.opf'
-    const opf = new OPF()
-    opf.title = data.title
+    const opf = new OPF.OPF()
+
+    properties.forEach((property) => {
+      const variable = config[property]
+      const propertyValue = data[variable || property]
+      if (propertyValue) opf[property] = propertyValue
+    })
+    if (config.identifiers) {
+      // check if identifiers are actually arrays
+      opf.identifiers = config.identifiers.map((id, index) => {
+        console.log('ok ok', id, data[id])
+        return {
+          id: index === 0,
+          scheme: id,
+          value: data[id]
+        }
+      })
+    }
+
     const filepath = path.join(dir, file)
-    mkdirp(config.directory, (err) => {
+    mkdirp(dir, (err) => {
       if (err) return reject(err)
       fs.writeFile(filepath, opf.toXML(), (e) => {
         if (e) return reject(e)

1d8079d755c9e6d6c3f6fd474cc46223e313c584
e-e-e <benjamin.mforster@gmail.com>
2017-11-26 19:59:53 +1100

    add fs escape option in addition to sanitize to tpl() and remove unneeded config options.

--- test/util.test.js
+++ test/util.test.js
@@ -33,38 +33,12 @@ test('tpl(string, object) selects replaces all ${x} in the with x from object', 
   t.end()
 })
 
-test('tpl(string, object, true) sanitizes the variables', (t) => {
-  const cases = [{
-    arguments: ['nothing to change', { var: 'simple' }],
-    output: 'nothing to change'
-  }, {
-    arguments: ['a ${var} test', { var: 'simple' }],
-    output: 'a simple test'
-  }, {
-    arguments: ['a ${var} test ${var}${var}', { var: 'duplicate' }],
-    output: 'a duplicate test duplicateduplicate'
-  },
-  {
-    arguments: ['a ${var} test with ${second}', { var: 'simple', second: 'many' }],
-    output: 'a simple test with many'
-  }, {
-    arguments: ['a ${var} test with ${second}', { var: 'simple', second: 'many' }],
-    output: 'a simple test with many'
-  }, {
-    arguments: ['a ${x.a} test with ${x[b]}', { x: { a: 'simple', b: 'paths' } }],
-    output: 'a simple test with paths'
-  }, {
-    arguments: ['now with a ${dead} path', { x: 'y' }],
-    output: 'now with a undefined path'
-  }, {
-    arguments: ['${dangerous}', { dangerous: 'a/a* ?/:2' }],
-    output: 'aa 2'
-  }]
-  cases.forEach(c => t.same(c.output, tpl(...c.arguments, true)))
+test('can also sanitizes the variables with ${path|safe}', (t) => {
+  t.same('aa 2', tpl('${dangerous|safe}', { dangerous: 'a/a*// ?/:2' }))
   t.end()
 })
 
-test('can also sanitizes the variables with ${path|save}', (t) => {
-  t.same('aa 2', tpl('${dangerous|safe}', { dangerous: 'a/a*// ?/:2' }))
+test('can also escapes the variables with ${path|fs}', (t) => {
+  t.same('a\\/filename\\/with\\/slashes', tpl('${dangerous|fs}', { dangerous: 'a/filename/with/slashes' }))
   t.end()
 })

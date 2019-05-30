047468686e96d84b94d1482193ea5ffd1ad50414
e-e-e <benjamin.mforster@gmail.com>
2019-05-30 21:21:32 +1000

    update deps / remove sqlite3

--- plugins/morph.js
+++ plugins/morph.js
@@ -1,59 +1,59 @@
-var sqlite3 = require('sqlite3').verbose()
+// var sqlite3 = require('sqlite3').verbose()
 
-/*
-  Config options
-  file: name of db - defaults to data.sqlite
-  refresh: bool - if true drops table if it already exists
-  table: name of table - defaults to data
-  columns: list of variable names to include as columns in the table
- */
+// /*
+//   Config options
+//   file: name of db - defaults to data.sqlite
+//   refresh: bool - if true drops table if it already exists
+//   table: name of table - defaults to data
+//   columns: list of variable names to include as columns in the table
+//  */
 
-let db
+// let db
 
-function start (instructions, options) {
-  return new Promise((resolve, reject) => {
-    try {
-      db = new sqlite3.Database(options.name || 'data.sqlite')
-      db.serialize(() => {
-        const table = options.table || 'data'
-        if (options.refresh) {
-          db.run(`DROP TABLE IF EXISTS ${table}`)
-        }
-        const columns = options.columns.join(' TEXT, ')
-        db.run(`CREATE TABLE IF NOT EXISTS ${table} (${columns} TEXT)`, () => {
-          resolve(instructions)
-        })
-      })
-    } catch (e) {
-      console.log('!!!!!!', e)
-      resolve(instructions)
-    }
-  })
-}
+// function start (instructions, options) {
+//   return new Promise((resolve, reject) => {
+//     try {
+//       db = new sqlite3.Database(options.name || 'data.sqlite')
+//       db.serialize(() => {
+//         const table = options.table || 'data'
+//         if (options.refresh) {
+//           db.run(`DROP TABLE IF EXISTS ${table}`)
+//         }
+//         const columns = options.columns.join(' TEXT, ')
+//         db.run(`CREATE TABLE IF NOT EXISTS ${table} (${columns} TEXT)`, () => {
+//           resolve(instructions)
+//         })
+//       })
+//     } catch (e) {
+//       console.log('!!!!!!', e)
+//       resolve(instructions)
+//     }
+//   })
+// }
 
-function saveData (data, options) {
-  const table = options.table || 'data'
-  return new Promise((resolve, reject) => {
-    try {
-      db.serialize(() => {
-        const columns = options.columns
-        const placeholders = Array(columns.length).fill('?').join(',')
-        const toInsert = columns.map(key => data[key])
-        var statement = db.prepare(`INSERT INTO ${table} (${columns.join(',')}) VALUES (${placeholders})`)
-        statement.run(...toInsert)
-        statement.finalize(() => {
-          resolve(data)
-        })
-      })
-    } catch (e) {
-      console.log('!!!!!!', e)
-      resolve(data)
-    }
-  })
-}
+// function saveData (data, options) {
+//   const table = options.table || 'data'
+//   return new Promise((resolve, reject) => {
+//     try {
+//       db.serialize(() => {
+//         const columns = options.columns
+//         const placeholders = Array(columns.length).fill('?').join(',')
+//         const toInsert = columns.map(key => data[key])
+//         var statement = db.prepare(`INSERT INTO ${table} (${columns.join(',')}) VALUES (${placeholders})`)
+//         statement.run(...toInsert)
+//         statement.finalize(() => {
+//           resolve(data)
+//         })
+//       })
+//     } catch (e) {
+//       console.log('!!!!!!', e)
+//       resolve(data)
+//     }
+//   })
+// }
 
-module.exports = {
-  onStart: start,
-  onData: saveData,
-  onEnd: a => a
-}
+// module.exports = {
+//   onStart: start,
+//   onData: saveData,
+//   onEnd: a => a
+// }
